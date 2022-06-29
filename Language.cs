48ddf87624ba00f65e0d654c99b71ce9e5c0674c
Pyrix25633 <biral.mattia@gmail.com>
2022-06-29 16:28:21 +0200

    translation test

---
+++ Language.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+
+namespace food_catalogue {
+    public class Language {
+        public static string language = "en-us";
+        public static Translation translation = new Translation();
+        public static void ReadTranslation() {
+            string fileContent = File.ReadAllText("translations/lang.json");
+            Lang? lang = JsonConvert.DeserializeObject<Lang>(fileContent);
+            if(lang != null && lang.language != null) language = lang.language;
+            fileContent = File.ReadAllText("translations/" + language + ".json");
+            Translation? temp = JsonConvert.DeserializeObject<Translation>(fileContent);
+            if(temp != null) translation = temp;
+        }
+    }
+
+    public class Translation {
+        public string? remove {get; set;}
+        public string? expired {get; set;}
+    }
+    public class Lang {
+        public string? language {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,50 @@
 using System;
 using Terminal.Gui;
+using NStack;
 
 namespace food_catalogue {
     public class Program {
         static void Main(string[] args) {
+            Language.ReadTranslation();
             Application.Init();
             Toplevel top = Application.Top;
-            View label = new Label("Label: ") {X = 3, Y = 2};
-            top.Add(label);
             MenuBar menu = new MenuBar(new MenuBarItem[] {
-                new MenuBarItem ("_Quit", "", () => {top.Running = false;})
+                new MenuBarItem ("Quit", "", () => {top.Running = false;})
 		    });
             top.Add(menu);
+            Window win = new Window("Window") {
+                X = 0, Y = 1,
+                Width = Dim.Fill(), Height = Dim.Fill()
+            };
+            top.Add(win);
+            //ListView expired = new ListView();
+            Button remove = new Button(0, 10, (ustring)Language.translation.remove);
+            remove.Clicked += RemoveItem;
+            void RemoveItem() {
+                //if(expired.Source.Length > 0)
+                    //expired.Source = Functions.RemoveAt(expired.Source, expired.SelectedItem);
+            }
+            win.Add(
+                new Label(0, 0, Language.translation.expired),
+                //expired,
+                remove
+            );
             Application.Run();
+            Application.Shutdown();
+        }
+    }
+
+    public class Functions {
+        public static ustring[] RemoveAt(ustring[] source, Int32 index) {
+            Int32 lenght = source.Length;
+            ustring[] dest = new ustring[lenght - 1];
+            if( index > 0 )
+                Array.Copy(source, 0, dest, 0, index);
+
+            if( index < lenght - 1 )
+                Array.Copy(source, index + 1, dest, index, lenght - index - 1);
+
+            return dest;
         }
     }
 }
